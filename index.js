5c0c340ac12acf415f34e38fce8822de8562afea
EvseevVD <evseevvd@sib-soft.ru>
2018-12-15 19:22:46 +0700

    create connector

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var SG = require('strong-globalize');
-SG.SetRootDir(__dirname);
-
 exports.initialize = function initializeDataSource(dataSource, callback) {
   console.log(dataSource.settings);
 };
